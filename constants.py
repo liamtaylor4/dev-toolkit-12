b72eac4cff087150f6d0f0dd7ed7726cda6bc4b1
Matej Hudák <liamtaylor4435@gmail.com>
2026-08-13 09:16:18 +0200

    add constant values for game setup

--- constants.py
+++ constants.py
@@ -1,5 +1,38 @@
-import time
+FPS_LIMIT = 60
 
-RETRY_COUNT = 3
-RETRY_DELAY = 2
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
+PLAYER_SPEED = 5
+ENEMY_SPEED = 3
+
+MAX_HEALTH = 100
+
+GRAVITY = 9.8
+
+COLORS = {
+    'BLACK': (0, 0, 0),
+    'WHITE': (255, 255, 255),
+    'RED': (255, 0, 0),
+    'GREEN': (0, 255, 0),
+    'BLUE': (0, 0, 255)
+}
+
+ASSETS_PATH = 'assets/'
+
+LEVELS = {
+    'level1': 'assets/levels/level1.json',
+    'level2': 'assets/levels/level2.json',
+}
+
+SOUND_EFFECTS = {
+    'jump': 'assets/sounds/jump.wav',
+    'hit': 'assets/sounds/hit.wav',
+}
+
+KEY_BINDINGS = {
+    'LEFT': 'a',
+    'RIGHT': 'd',
+    'UP': 'w',
+    'DOWN': 's'
+}
