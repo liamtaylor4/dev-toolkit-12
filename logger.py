8edc0ae2db42a402cf91d6ff9028fe93726cb058
Matej Hudák <liamtaylor4435@gmail.com>
2026-08-19 09:15:57 +0200

    update logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,15 +1,11 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
-def setup_logger(name, log_file, level=logging.INFO):
-    handler = RotatingFileHandler(log_file, maxBytes=5*1024*1024, backupCount=3)
+def setup_logger(log_file, max_bytes=10485760, backup_count=5):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
-
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
     logger.addHandler(handler)
-
     return logger
-
-logger = setup_logger('game_logger', 'game.log')
